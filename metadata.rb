353c61b1318e63a46a84cc2ed1fe28c39381345b
Adam Krone <krone.adam@gmail.com>
2013-10-23 13:42:37 -0500

    Adds metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name								"deployd"
+maintainer					"Adam Krone"
+maintainer_email		"krone.adam@gmail.com"
+license							"Apache 2.0"
+description					"Installs and configures deployd"
+version							"0.1.0"
+
+depends "nodejs"
