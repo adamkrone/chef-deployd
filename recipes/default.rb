f930ecf73b206df7f41f180680bf5405f9d8bba3
Adam Krone <krone.adam@gmail.com>
2013-10-27 17:17:31 -0500

    Adds deployd user that owns the global deployd install

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,29 +6,45 @@
 # Authors:
 # 		Adam Krone <krone.adam@gmail.com>
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to 
-# deal in the Software without restriction, including without limitation the 
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-# sell copies of the Software, and to permit persons to whom the Software is 
-# furnished to do so, subject to the following conditions:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
-# IN THE SOFTWARE.
-
+# Install dependecies
 include_recipe "mongodb::10gen_repo"
 include_recipe "mongodb"
 include_recipe "nodejs"
 
-execute "npm install -g deployd" do
-	command "npm install -g deployd"
+# Add deployd system user
+execute "add #{node['deployd']['user']} user" do
+	command "useradd -m -s /bin/bash #{node['deployd']['user']}"
+	action :run
+	not_if "cat /etc/passwd | grep #{node['deployd']['user']}"
+end
+
+# Install deployd with npm
+execute "install deployd with npm" do
+	command "sudo npm install -g deployd"
+	action :run
+end
+
+# Change permissions for dpd
+execute "chown dpd" do
+	command "sudo chown #{node['deployd']['user']}: /usr/local/bin/dpd"
+	action :run
+end
+
+# Change permissions for deployd lib
+execute "chown deployd lib" do
+	command "sudo chown -R #{node['deployd']['user']}: /usr/local/lib/node_modules/deployd"
 	action :run
 end
