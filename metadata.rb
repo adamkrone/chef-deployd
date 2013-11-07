2d0c5ad607a89444ac446bb5b515b51227e88292
Adam Krone <krone.adam@gmail.com>
2013-11-07 12:36:41 -0600

    Increment version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer					"Adam Krone"
 maintainer_email		"krone.adam@gmail.com"
 license							"Apache 2.0"
 description					"Installs and configures deployd"
-version							"0.4.0"
+version							"0.4.1"
 
 depends "nodejs"
 depends "mongodb"
